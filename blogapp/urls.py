58156c9dee243950672ef7392b903c487883a42b
FerMiguelao <fernandomiguelao@gmail.com>
2018-11-10 01:32:55 -0200

    Backend funcionando.

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('post/<int:pk>/', views.post_completo, name='post_completo'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def home(request):
-    return render(request, 'blogapp/home.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blogapp/home.html', {'posts':posts})
+
+def post_completo(request, pk):
+    posts = Post.objects.all()
+    post_c = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogapp/post_completo.html', {'post_c':post_c,'posts':posts})
